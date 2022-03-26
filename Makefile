7569919b079c7b71f4bb0a428763e211a0af9130
Arnisfet <mrudge@student.21-school.ru>
2022-03-26 19:23:22 +0300

    База проекта

--- Makefile
+++ Makefile
@@ -64,7 +64,7 @@ $(MLX):
 
 #Project file rule
 $(NAME): $(BUILDOBJS)
-	$(CC) $(BUILDOBJS) $(MLX) $(MLXFLAGS) $(DEBUG) $(CFLAGS) -o $(NAME)
+	$(CC) $(BUILDOBJS) $(LIBFT) $(MLX) $(MLXFLAGS) $(DEBUG) $(CFLAGS) -o $(NAME)
 
 # Cleaning up the build files
 clean:
--- src/main.c
+++ src/main.c
@@ -1,1 +1,15 @@
-int main() {}
+#include "../inc/Cube3d.h"
+
+int main()
+{
+	void	*mlx;
+	void	*mlx_win;
+	char	a[] = "qwerty";
+
+	int i = ft_strlen(a);
+	i--;
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
+	mlx_win--;
+}
