990f860bbcc0e2952b8a5aa648e4316d2daea4e8
Arnisfet <mrudge@student.21-school.ru>
2022-03-26 19:23:22 +0300

    База проекта

--- src/main.c
+++ src/main.c
@@ -1,1 +1,11 @@
-int main() {}
+#include "../inc/Cube3d.h"
+
+int main()
+{
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
+}
